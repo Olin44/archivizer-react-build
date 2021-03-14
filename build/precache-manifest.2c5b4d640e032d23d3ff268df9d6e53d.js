afa7b769d3639ee64aad4150fc15ab5ac25ed67c
Jacek Ciesielka <ciesielka@agh.edu.pl>
2021-03-14 17:48:32 +0100

    Initial commit

---
+++ build/precache-manifest.2c5b4d640e032d23d3ff268df9d6e53d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60f4ca9cb8aeb263a35549cf6bd9e56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5caa719333684cb53598",
+    "url": "/static/css/2.5acf4641.chunk.css"
+  },
+  {
+    "revision": "92489b412e0c629910d8",
+    "url": "/static/css/main.90566059.chunk.css"
+  },
+  {
+    "revision": "5caa719333684cb53598",
+    "url": "/static/js/2.e0b28a4c.chunk.js"
+  },
+  {
+    "revision": "898b9371e7cc2868a317f1a2fdfa913f",
+    "url": "/static/js/2.e0b28a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92489b412e0c629910d8",
+    "url": "/static/js/main.aed07aa6.chunk.js"
+  },
+  {
+    "revision": "9e4a0d7c934118ec9cfe",
+    "url": "/static/js/runtime-main.794a6e05.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
